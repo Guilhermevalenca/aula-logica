093ad3e5db53e0738802d8731a0a6ddd400387a3
Guilherme valença <guilherm3vrp@gmail.com>
2024-08-29 11:33:03 -0300

    up

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	class Coordenada {
+		linha: number;
+		coluna: number;
+	}
+	const jogador = new Coordenada();
+	jogador.linha = 0;
+	jogador.coluna = 2;
+
+	const mapa: number[][] = [
+		[0,0,0,0],
+		[0,0,0,0],
+		[0,0,0,0],
+	];
+	function keydown(event: KeyboardEvent) {
+		if(event.key === 'w') {
+			jogador.linha--;
+		} else if(event.key === 's') {
+			jogador.linha++;
+		} else if(event.key === 'd') {
+			jogador.coluna++;
+		} else if(event.key === 'a') {
+			jogador.coluna--;
+		}
+	}
+</script>
+<svelte:window on:keydown={keydown} />
+<table>
+	{#each mapa as linhas, i}
+	<tr
+		style="display: flex;"
+	>
+		{#each linhas as colunas, j}
+			{#if jogador.coluna === j && jogador.linha === i}
+				<td
+					style="width: 16px; height: 16px;
+					background-color: black;"
+				></td>
+			{:else}
+				<td
+					style="width: 16px; height: 16px;background-color: aqua"
+				></td>
+			{/if}
+		{/each}
+	</tr>
+	{/each}
+</table>
---
+++ src/routes/joao/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let escrevendo: string = '';
+</script>
+
+<h1 style="background-color: {escrevendo === 'ola' ? 'aqua' : 'red'};">{escrevendo}</h1>
+
+<input bind:value={escrevendo} >
